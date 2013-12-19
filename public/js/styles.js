4e6dc6d5315ee847e7ae81619ec866310ae0e541
bessog <gbesso@gopivotal.com>
2013-12-19 22:05:44 +0000

    added style save

--- public/js/styles.js
+++ public/js/styles.js
@@ -1,9 +1,27 @@
+var saveStyle = function(site) {
+  data = {'css': $('#css').val()};
+  $.ajax({
+    type: "PUT",
+    url: '/style/'+ $('#styleselect').val(),
+    contentType: "application/json",
+    data: data,
+    success: function(ret) {
+      $("style").remove();
+      $("body").prepend("<style>"+data.css+"</style>");
+      $("#css").empty();
+      $("#css").val(data.css);
+      $('.banner').css('background-color','');
+    }
+  });
+}
+
 $(document).ready(function () {
 
   $.getJSON("/styleselect", function(data) {
     for(var i = 0; i < data.length; i++) {
       $("#styleselect").append('<option>' + data[i] + '</option>');
     }
+    $('.banner').css('background-color','');
   });
 
   $.getJSON("/style/default", function(data) {
@@ -21,12 +39,14 @@ $(document).ready(function () {
       $("body").prepend("<style>"+data.css+"</style>");
       $("#css").empty();
       $("#css").val(data.css);
+      $('.banner').css('background-color','');
     });
   });
 
   $("#css").keyup(function() {
     $("style").remove();
     $("body").prepend("<style>"+$("#css").val()+"</style>");
+    $('.banner').css('background-color','#faa');
   });
 
 });
